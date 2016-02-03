c800b42011d7267a19bb30dade8fef34b60fe138
Hongbo Miao <Hongbo.Miao@outlook.com>
2016-02-03 08:08:03 -0500

    update path

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var PATH_TO_KEY = "",
-    PATH_TO_CERT = "";
-    PATH_TO_CHAIN = "";
+var PATH_TO_KEY = "/Users/hongbo-miao/Clouds/Git/ssl/server.key",
+    PATH_TO_CERT = "/Users/hongbo-miao/Clouds/Git/ssl/server.crt";
+    //PATH_TO_CHAIN = "";
 
 var fs = require('fs'),
     httpProxy = require('http-proxy');
@@ -9,9 +9,9 @@ var options = {
   ssl: {
     key: fs.readFileSync(PATH_TO_KEY, 'utf8'),
     cert: fs.readFileSync(PATH_TO_CERT, 'utf8'),
-    ca : fs.readFileSync(PATH_TO_CHAIN, 'utf8')
+    //ca : fs.readFileSync(PATH_TO_CHAIN, 'utf8')
   },
-  target : "http://localhost",
+  target : "http://localhost:3000",
   ws: true,
   xfwd: true
 };
